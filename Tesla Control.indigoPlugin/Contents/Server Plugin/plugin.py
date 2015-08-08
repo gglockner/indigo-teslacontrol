84b07ea38e868cba1c62b37ccab7e44b26fb9864
Greg Glockner <greg@glockners.net>
2015-08-07 17:56:58 -0700

    Get list of cars for plugin

--- Tesla Control.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Tesla Control.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -15,6 +15,7 @@ class Plugin(indigo.PluginBase):
 	########################################
 	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
 		indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
+		self.vehicles = []
 		self.debug = True
 
 	def __del__(self):
@@ -24,8 +25,22 @@ class Plugin(indigo.PluginBase):
 	def startup(self):
 		# self.debugLog(u"startup called")
 		self.debugLog("Username: %s" % self.pluginPrefs['username'])
-		pass
+		# TODO: exception handling
+		connection = teslajson.Connection(self.pluginPrefs['username'],
+										  self.pluginPrefs['password'])
+		self.vehicles = connection.vehicles
+		self.debugLog("%i vehicles found" % len(connection.vehicles))
 
 	def shutdown(self):
 		# self.debugLog(u"shutdown called")
 		pass
+	
+	def carListGenerator(self, filter="", valuesDict=None, typeId="", targetId=0):
+		# From the example above, filter = “stuff”
+		# You can pass anything you want in the filter for any purpose
+		# Create an array where each entry is a list - the first item is
+		# the value attribute and last is the display string that will 
+		# show up in the control. All parameters are read-only.
+# 		myArray = [("option1", "First Option"),("option2","Second Option")]
+# 		return myArray
+		return [(str(v['id']), "%s (%s)" % (v['display_name'], v['vin'])) for v in self.vehicles]
